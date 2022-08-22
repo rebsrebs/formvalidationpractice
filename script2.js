1b5722a48d322136f2a5c019b86be2445c9e8f8b
“Rebecca <rebeccaschiffman@gmail.com>
2022-08-22 13:19:55 -0700

    Cleanup code

--- script2.js
+++ script2.js
@@ -9,87 +9,65 @@ const passwordError = document.getElementById('passwordError');
 const confirmPassword = document.getElementById('confirmPassword');
 const confirmPasswordError = document.getElementById('confirmPasswordError');
 
-
-
-// CHECKING ON INPUT
-emailAddress.addEventListener('input', (event) => {
-  // Each time the user types something, we check if the
-  // form fields are valid.
+// CHECKING EMAIL ON INPUT
+emailAddress.addEventListener('input', () => {
   // if the email address is valid:
   if (emailAddress.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
+    // remove error message
     emailError.textContent = ''; // Reset the content of the message
     emailError.className = 'error'; // Reset the visual state of the message
   } else {
     // If there is still an error, show the correct error
     emailShowError();
   }
-});
-
+})
 
 // CHECK FOR COUNTRY TO SHOW OR HIDE ZIPCODE INPUT
 country.addEventListener('input', () => {
+  // if a country is selected
   if (country.value != "selectCountry") {
+    // show the zipcode input
     zipcode.classList.remove('hidden');
     zipcodeError.classList.remove('hidden');
   } else {
+    // otherwise hide the zipcode input
     zipcode.classList.add('hidden');
     zipcodeError.classList.add('hidden');
   }
 })
 
-
 // CHECKING ZIPCODE ON INPUT
 zipcode.addEventListener('input', (event) => {
   // get current zip code input value
   let userZipCode = document.getElementById("zipcode").value;
-  
   // create regex expressions
   var usmx = new RegExp(/(^\d{5}$)|(^\d{5}-\d{4}$)/);
   var can = new RegExp(/^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i);
-  
   // if country is US or Mexico
   if (country.value === "unitedStates" || country.value === "mexico") {
-    console.log('country is US or Mexico');
-    console.log(userZipCode);
-
     // if zip code fails regex test show error message
     if (!usmx.test(userZipCode.toString())) {
       zipcodeError.textContent = 'Please enter a valid zip code.';
       zipcodeError.className = 'error active';  
-    } 
-      else {
-        console.log('no error');
-        zipcodeError.textContent = ''; // Reset the content of the message
-        zipcodeError.className = 'error'; // Reset the visual state of the message
-      }
-  } else if (country.value === "canada") {
-    if (!can.test(userZipCode.toString())) {
-      zipcodeError.textContent = 'Please enter a valid Canadian zip code.';
-    zipcodeError.className = 'error active';
+    } else {
+      zipcodeError.textContent = ''; // Reset the content of the message
+      zipcodeError.className = 'error'; // Reset the visual state of the message
     }
-      else {
-        console.log('no error');
-        zipcodeError.textContent = ''; // Reset the content of the message
-        zipcodeError.className = 'error'; // Reset the visual state of the message
-      }
+  // if country is canada
+  } else if (country.value === "canada") {
+    // if zip code fails test
+    if (!can.test(userZipCode.toString())) {
+      // show error message
+      zipcodeError.textContent = 'Please enter a valid Canadian zip code.';
+      zipcodeError.className = 'error active';
+    } else {
+      console.log('no error');
+      zipcodeError.textContent = ''; // Reset the content of the message
+      zipcodeError.className = 'error'; // Reset the visual state of the message
+    }
   } 
 }
 );
-
-
-
-// CHECKING ON SUBMIT
-form3.addEventListener('submit', (event) => {
-  // if the email field is valid, we let the form submit
-  if (!emailAddress.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    emailShowError();
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
-  }
-});
 
 function emailShowError() {
   if (emailAddress.validity.valueMissing) {
@@ -109,20 +87,7 @@ function emailShowError() {
   emailError.className = 'error active';
 }
 
-// function zipcodeShowError() {
-//   if (zipcode.validity.valueMissing) {
-//     zipcodeError.textContent = 'Please enter a zip code';
-//   } else if () {
-
-//   }
-//     // If the field is empty,
-//     // display the following error message.
-//   zipcodeError.textContent = 'Please enter a valid Canadian zip code';
-//   zipcodeError.className = 'error active';
-// }
-
-
-// CHECKING ON INPUT
+// CHECKING PASSWORD ON INPUT
 password.addEventListener('input', (event) => {
   // Each time the user types something, we check if the
   // form fields are valid.
@@ -139,13 +104,10 @@ password.addEventListener('input', (event) => {
   }
 });
 
-// CHECKING ON INPUT
+// CHECKING CONFIRM PASSWORD ON INPUT
 confirmPassword.addEventListener('input', (event) => {
   console.log(`password.value is ${password.value}`);
   console.log(`confirmPassword.value is ${confirmPassword.value}`);
-  
-  // Each time the user types something, we check if the
-  // form fields are valid.
   // if the email address is valid:
   if (confirmPassword.value != password.value) {
     // If there is still an error, show the correct error
@@ -160,23 +122,32 @@ confirmPassword.addEventListener('input', (event) => {
 });
 
 // eye buttons
-
 const eyes = document.querySelectorAll('.showhide');
-
- for (let i = 0; i < eyes.length; i++) {
-     eyes[i].addEventListener("click", function(event) {
+  for (let i = 0; i < eyes.length; i++) {
+    eyes[i].addEventListener("click", function(event) {
       let id = event.target.getAttribute("data-pwd");
-  let targetIcon = event.target;
-  let targetInput = document.getElementById(id)
-  console.log(id);
-  console.log(targetInput);
-  console.log(targetInput.type);
-  if (targetInput.type=='password') {
-    targetInput.type='text';
-    targetIcon.src='./images/eye-off.svg';
-  } else if (targetInput.type=='text') {
-    targetInput.type='password';
-    targetIcon.src='./images/eye.svg';
+      let targetIcon = event.target;
+      let targetInput = document.getElementById(id)
+      console.log(id);
+      console.log(targetInput);
+      console.log(targetInput.type);
+      if (targetInput.type=='password') {
+        targetInput.type='text';
+        targetIcon.src='./images/eye-off.svg';
+      } else if (targetInput.type=='text') {
+        targetInput.type='password';
+        targetIcon.src='./images/eye.svg';
+      }
+    });
   }
-     });
- }
+
+// CHECKING ON SUBMIT
+form3.addEventListener('submit', (event) => {
+  // if the email field is valid, we let the form submit
+  if (!emailAddress.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    emailShowError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
